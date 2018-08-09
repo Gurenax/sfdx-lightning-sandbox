f53a85b6d4d4c7d13c90cf2a36771bb4cdf24c26
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2018-08-09 17:01:08 +1000

    Update PromiseComponent  Controller to call getAllUsersPromise

--- force-app/main/default/aura/PromiseComponent/PromiseComponentController.js
+++ force-app/main/default/aura/PromiseComponent/PromiseComponentController.js
@@ -1,5 +1,16 @@
 ({
-    myAction : function(component, event, helper) {
-
+    // Client Side controller function name cannot be the same with the Server Side controller method name
+    // e.g. if the apex class method is getUsers, this function should be named something else (i.e. getUsersList)
+    getUsersList : function(component, event, helper) {
+        helper.getAllUsersPromise(component).then(
+            $A.getCallback(result => {
+                console.log('allUsers', result)
+                component.set('v.users', result)
+            }),
+            $A.getCallback(error => {
+                // Something went wrong
+                console.error('An error occurred getting the account : ' + error.message);
+            })
+        )
     }
 })
