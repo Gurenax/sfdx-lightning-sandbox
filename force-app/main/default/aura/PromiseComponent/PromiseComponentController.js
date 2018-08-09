97da6589f8740d1cc5d2e369f9483f217bc61c7c
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2018-08-09 20:57:03 +1000

    Add lightningPromisify

--- force-app/main/default/aura/PromiseComponent/PromiseComponentController.js
+++ force-app/main/default/aura/PromiseComponent/PromiseComponentController.js
@@ -2,15 +2,13 @@
     // Client Side controller function name cannot be the same with the Server Side controller method name
     // e.g. if the apex class method is getUsers, this function should be named something else (i.e. getUsersList)
     getUsersList : function(component, event, helper) {
-        helper.getAllUsersPromise(component).then(
-            $A.getCallback(result => {
-                console.log('allUsers', result)
-                component.set('v.users', result)
-            }),
-            $A.getCallback(error => {
-                // Something went wrong
-                console.error('An error occurred getting the account : ' + error.message);
-            })
-        )
+        helper.lightningPromisify(component, helper.getAllUsersPromise).then(result => {
+            console.log('allUsers', result)
+            component.set('v.users', result)
+        })
+        .catch(error => {
+            // Error in Promise
+            console.error('An error in Promise occurred getting the account : ' + error.message);
+        })
     }
 })
