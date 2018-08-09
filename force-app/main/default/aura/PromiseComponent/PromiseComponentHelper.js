1f0df0149e7ac76de57aaf4fc8779b6714b920d5
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2018-08-09 17:02:19 +1000

    Add getAllUsersPromise

--- force-app/main/default/aura/PromiseComponent/PromiseComponentHelper.js
+++ force-app/main/default/aura/PromiseComponent/PromiseComponentHelper.js
@@ -1,5 +1,28 @@
 ({
-    helperMethod : function() {
+    getAllUsersPromise : function(component) {
+        // Get Apex Controller Method
+        const action = component.get('c.getAllUsers')
 
+        return new Promise((resolve, reject) => {
+            // Set parameters if needed
+            // action.setParams({})
+
+            // Buikd the Action
+            action.setCallback(this, response => {
+                const state = response.getState()
+
+                if(state === 'SUCCESS') {
+                    const allUsers = response.getReturnValue()
+                    resolve(allUsers)
+                } else {
+                    const errors = response.getError()
+                    reject(errors)
+                }
+            })
+
+            // Perform Action
+            $A.enqueueAction(action)
+        
+        })
     }
 })
