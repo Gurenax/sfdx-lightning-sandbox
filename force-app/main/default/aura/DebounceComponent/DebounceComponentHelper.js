c23a63561381f00b3662b0ba862d1e77c070208d
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2018-08-10 16:56:47 +1000

    Add lightningDebounce and increaseNumber

--- force-app/main/default/aura/DebounceComponent/DebounceComponentHelper.js
+++ force-app/main/default/aura/DebounceComponent/DebounceComponentHelper.js
@@ -1,5 +1,16 @@
 ({
-    helperMethod : function() {
+    lightningDebounce : function(fn, ms) {
+        const m = !ms ? 0 : ms
+        return function() {
+            const args = arguments
+            clearTimeout(window.timeoutId)
+            window.timeoutId = setTimeout(() => fn.apply(this, args), m)
+        }
+    },
 
+    increaseNumber : function(component, num) {
+        const val = component.get('v.debounceCounterValue')
+        component.set('v.debounceCounterValue', val + num)
     }
+
 })
