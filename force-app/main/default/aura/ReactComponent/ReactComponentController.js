f41ddf8573a533a14ce16cacfb402a2510604edd
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2018-08-16 15:50:44 +1000

    Update ReactComponentController

--- force-app/main/default/aura/ReactComponent/ReactComponentController.js
+++ force-app/main/default/aura/ReactComponent/ReactComponentController.js
@@ -1,31 +1,35 @@
 ({
+    doInit: function(component, event, helper) {
+        console.log('>>> doInit')
+    },
+
     reactBeforeLoad: function(component, event, helper) {
         console.log('reactBeforeLoad')
     },
 
     reactInit : function(component, event, helper) {
         console.log('reactInit')
-        // var dataService = {
-        //     findAll : $A.getCallback(function(callback) {
-        //         var action = component.get("c.findAll");
-        //         action.setCallback(this, function(a) {
-        //             var contacts = a.getReturnValue();
-        //             callback(contacts);
-        //         });
-        //         $A.enqueueAction(action, false);
-        //     }),
-        //     findByName : $A.getCallback(function(name, callback) {
-        //         var action = component.get("c.findByName");
-        //         action.setParams({name: name});
-        //         action.setCallback(this, function(a) {
-        //             var contacts = a.getReturnValue();
-        //             callback(contacts);
-        //         });
-        //         $A.enqueueAction(action, false);
-        //     })
-        // }
         
-        const dataService = {}
+        const dataService = {
+            findAll : $A.getCallback(function(callback) {
+                const action = component.get("c.findAll");
+                action.setCallback(this, function(a) {
+                    const contacts = a.getReturnValue()
+                    callback(contacts)
+                })
+                $A.enqueueAction(action, false)
+            }),
+            findByName : $A.getCallback(function(name, callback) {
+                const action = component.get("c.findByName")
+                action.setParams({name: name})
+                action.setCallback(this, function(a) {
+                    const contacts = a.getReturnValue()
+                    callback(contacts)
+                })
+                $A.enqueueAction(action, false)
+            })
+        }
+        
         const container = component.find("container").getElement()
 
         reactApp.init(container, dataService)
