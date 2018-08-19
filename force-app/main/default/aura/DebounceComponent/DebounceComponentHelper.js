6bfd30329415ec686d16261091e98f10b5c99afd
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2018-08-19 14:44:26 +1000

    Update debounce component

--- force-app/main/default/aura/DebounceComponent/DebounceComponentHelper.js
+++ force-app/main/default/aura/DebounceComponent/DebounceComponentHelper.js
@@ -11,6 +11,18 @@
     increaseNumber : function(component, num) {
         const val = component.get('v.debounceCounterValue')
         component.set('v.debounceCounterValue', val + num)
-    }
+    },
+
+    /**
+     * Sameed's Debounce - Need to test this
+     */
+    debounce: function (fn, delayInMs) {
+        const delay = !delayInMs ? 0 : delayInMs
+        const timeoutId = fn.toString(); // generating a unique string by stringifying the function
+        return (...args) => {
+            clearTimeout(window[timeoutId])
+            window[timeoutId] = setTimeout($A.getCallback(() => fn.apply(this, args)), delay)
+        }
+    },
 
 })
